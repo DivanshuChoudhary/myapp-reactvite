086b17e89993feb0acb56e45720c5b5b0512427b
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-23 13:30:26 +0530

    project section added

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <h2>About Me</h2>
 
         <p>
-          I am a B.Tech student learning DevOps, Git and Github .
+          I am a B.Tech CSE student learning DevOps, Git and Github .
         </p>
 
         <div className="skills">
@@ -26,6 +26,38 @@ function App() {
           <li>CSS</li>
           <li>Git & GitHub</li>
          </ul>
+
+         <div className="projects">
+  <h2>Projects</h2>
+
+  <div className="project-card">
+    <h3>Netflix Clone UI</h3>
+    <p>
+      Built a responsive Netflix-inspired user interface using HTML and CSS.
+    </p>
+  </div>
+
+  <div className="project-card">
+    <h3>TravelX Landing Page</h3>
+    <p>
+      Modern travel website landing page with responsive design.
+    </p>
+  </div>
+
+  <div className="project-card">
+    <h3>Cloud Landing Page</h3>
+    <p>
+      Cloud services landing page focused on clean UI and layout design.
+    </p>
+  </div>
+
+  <div className="project-card">
+    <h3>Cricket Score Page</h3>
+    <p>
+      Cricket score display page with match information layout.
+    </p>
+  </div>
+</div>
         </div>
       </div>
     </div>
